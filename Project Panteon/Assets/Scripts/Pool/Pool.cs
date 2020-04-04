683af7f7cde95aec1f7418fa7723c1b0758974db
nkflipflop <48224336+nkflipflop@users.noreply.github.com>
2020-04-05 00:21:36 +0300

    object pooling is done

--- Project Panteon/Assets/Scripts/Pool/Pool.cs
+++ Project Panteon/Assets/Scripts/Pool/Pool.cs
@@ -8,19 +8,20 @@ public class Pool : MonoBehaviour
     public string poolName;                                         // Pool Name
     public GameObject poolCellPrefab;                               // One cell of the pool
 
-    private GameObject _rootObj;                                    // Root for unused obj
+    private GameObject _parent;                                     // Root for unused obj
     private Stack<PoolObject> _poolStack = new Stack<PoolObject>(); // Stack for pool
 
     // Initiliazes the pool with given name
-    public void InitPool() {
-        _rootObj = new GameObject(poolName + " Pool");
+    public void InitPool(Transform parent) {
+        _parent = new GameObject(poolName);
+        _parent.transform.SetParent(parent);
 
         // First Push
         PoolObject poolObject = GameObject.Instantiate(poolCellPrefab).AddComponent<PoolObject>();
         PushObject(poolObject);
 
         // Filling the pool with first object
-        for (int i = 0; i < 50; i++) {
+        for (int i = 0; i < 25; i++) {
             poolObject = GameObject.Instantiate(poolObject);
             PushObject(poolObject);
         }
@@ -37,7 +38,7 @@ public class Pool : MonoBehaviour
         poolObject.gameObject.SetActive(false);
         poolObject.gameObject.name = "Cell";
         _poolStack.Push(poolObject);
-        poolObject.gameObject.transform.SetParent(_rootObj.transform, false);
+        poolObject.gameObject.transform.SetParent(_parent.transform, false);
     }
 
     // Pops object from the pool stack
